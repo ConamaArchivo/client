d54dd90c4e8f42510c947ee092d506e787ce4479
Jan Cernik <jancernik12@gmail.com>
2022-08-21 19:18:20 -0300

    Place API url in env variable

--- src/axios.js
+++ src/axios.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
-// const API_URL = 'https://conama-server.herokuapp.com';
-const API_URL = 'http://localhost:5000';
 
 export const axiosPublic = axios.create({
-  baseURL: API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
